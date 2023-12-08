e5f198d04f0f5fc78dbdfd919010b9c41e015bcc
Gayoung <gayoung801@naver.com>
2023-12-08 09:03:00 +0900

    Feat : ConcatExample.java

---
+++ src/main/java/ggumi/maven/example/solution/ConcatExample.java
@@ -0,0 +1,16 @@
+package ggumi.maven.example.solution;
+
+import java.util.Scanner;
+
+public class ConcatExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.next();
+        String b = scanner.next();
+        System.out.println(a+b);
+    }
+}
+/**
+ * Scanner scanner = new Scanner(System.in)
+ * String a = scanner.next();
+ */
